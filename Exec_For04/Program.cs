7b1115623d8f2f775f5cd3612286196d780a50be
w2192 <w2192@LAPTOP-2BO0FNQF>
2022-10-17 00:15:38 +0800

    complete

---
+++ Exec_For04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For04
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		//呈現1~20, 若是3的倍數顯示 foo, 若是5的倍數顯示bar, 若是15
+		//的倍數顯示foobar, 其餘顯示數值
+			for (int i = 1;i<=20;i++)
+			{
+				if(i%3==0 && i%5==0)
+				{
+					Console.WriteLine("foobar");
+				}
+				else if(i%3==0)
+				{
+					Console.WriteLine("foo");
+				}
+				else if(i%5==0)
+				{
+					Console.WriteLine("bar");
+				}
+				Console.WriteLine(i);
+			}
+
+		}
+	}
+}
