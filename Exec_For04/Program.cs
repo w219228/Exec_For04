187962ea16ac41204792451f8faf602e2ce9c2a7
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 11:55:52 +0800

    complete

--- Exec_For04/Program.cs
+++ Exec_For04/Program.cs
@@ -10,25 +10,28 @@ namespace Exec_For04
 	{
 		static void Main(string[] args)
 		{
-		//呈現1~20, 若是3的倍數顯示 foo, 若是5的倍數顯示bar, 若是15
-		//的倍數顯示foobar, 其餘顯示數值
-			for (int i = 1;i<=20;i++)
+			//呈現1~20, 若是3的倍數顯示 foo, 若是5的倍數顯示bar, 若是15
+			//的倍數顯示foobar, 其餘顯示數值
+			for (int i = 1; i <= 20; i++)
 			{
-				if(i%3==0 && i%5==0)
+				if (i % 3 == 0 && i % 5 == 0)
 				{
 					Console.WriteLine("foobar");
 				}
-				else if(i%3==0)
+				else if (i % 3 == 0)
 				{
 					Console.WriteLine("foo");
 				}
-				else if(i%5==0)
+				else if (i % 5 == 0)
 				{
 					Console.WriteLine("bar");
 				}
-				Console.WriteLine(i);
-			}
+				else
+				{
+					Console.WriteLine(i);
+				}
 
+			}
 		}
 	}
 }
